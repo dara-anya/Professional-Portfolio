7c93a06145935e68037b8a37d7a93f7209a7523e
Dara Anya <daraanya@gmail.com>
2019-04-07 01:53:27 -0500

    Main: Section One

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-    
+
+    /****Introduction Sequence****/
     $("#name").css("display", "none");
     $("#welcome").css("display", "none");
 
@@ -10,7 +11,7 @@ $(document).ready(function(){
 
     setTimeout(function(){
         $("#name").css("display", "none");
-    },2000);
+    },2500);
 
     setTimeout(function(){
         $("#welcome").fadeIn(2000);
@@ -19,4 +20,47 @@ $(document).ready(function(){
     setInterval(function(){
         $("#welcome").fadeOut(2000);
     },4000);
+
+    /****Main Sequence****/
+    $("#line1").css("display", "none");
+    $("#line2").css("display", "none");
+    $("#line3").css("display", "none");
+    $("#line4").css("display", "none");
+
+    $("#header").css("display", "none");
+    $("#position").css("display", "none");
+    $("#summary").css("display", "none");
+    $("#headshot").css("display", "none");
+
+    $("#email").css("display", "none");
+    $("#phone").css("display", "none");
+    $("#location").css("display", "none");
+    $("#linkedin").css("display", "none");
+    $("#github").css("display", "none");
+    
+    setInterval(function(){
+        //$("#line1").css("display", "block");
+        //$("#line2").css("display", "block");
+
+        $("#header").fadeIn(1000);
+        $("#position").fadeIn(2000);
+        $("#summary").fadeIn(3000);
+        $("#headshot").fadeIn(4000);
+    },7000);
+    setInterval(function(){
+        $("#email").fadeIn(1000);
+    },9500);
+    setInterval(function(){
+        $("#phone").fadeIn(1000);
+    },10000);
+    setInterval(function(){
+        $("#location").fadeIn(1000);
+    },10500);
+    setInterval(function(){
+        $("#linkedin").fadeIn(1000);
+    },11000);
+    setInterval(function(){
+        $("#github").fadeIn(1000);
+    },11500);
+
 });
